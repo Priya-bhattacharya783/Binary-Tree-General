238896317946b76b7eb2b55783c7a06d336a6ba3
Priya-bhattacharya783 <priyabhattacharya2005@gmail.com>
2025-06-28 15:43:28 +0530

    Add files via upload

---
+++ same_tree.java
@@ -0,0 +1,29 @@
+class same_tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+
+    // Loop till the queue is not empty
+        while (!queue.isEmpty()) {
+           TreeNode first = queue.poll();
+           TreeNode second = queue.poll();
+
+           // Check for equality
+           if (first == null && second == null) {
+                continue;
+            } 
+            else if (first == null || second == null || first.val != second.val)
+            {
+                return false;
+            }
+
+            // Add other nodes
+             queue.add(first.left);
+             queue.add(second.left);
+             queue.add(first.right);
+             queue.add(second.right);
+        }
+     return true;
+    }
+}
